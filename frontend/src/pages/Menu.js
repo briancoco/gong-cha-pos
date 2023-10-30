c913372fe3891dd27c74e57eb0d9ed706c8116de
briancoco <briannguyencoco@gmail.com>
2023-10-30 17:08:43 -0500

    Finsished menu page

--- frontend/src/pages/Menu.js
+++ frontend/src/pages/Menu.js
@@ -1,8 +1,52 @@
 import React from 'react'
+import MilkTea from '../assets/milk-tea.jpg';
+import TeaLatte from '../assets/tea-latte.jpg';
+import Slush from '../assets/slush.jpg';
+import CreativeMix from '../assets/creative-mix.png';
+import brewedTea from '../assets/brewed-tea.jpg';
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className='menu'>
+      <section className='menu-category-container'>
+        <img className='menu-category-img' src={MilkTea} alt='Milk Tea' />
+        <section className='menu-category-drinks'>
+          <h2 className='category-header'>Milk Tea</h2>
+          <div className='menu-item'>
+            <p>Black Milk Tea</p>
+            <p>$5.99</p>
+          </div>
+          <div className='menu-item'>
+            <p>Brown Sugar Milk Tea</p>
+            <p>$5.99</p>
+          </div>
+          <div className='menu-item'>
+            <p>Caramel Milk Tea</p>
+            <p>$5.99</p>
+          </div>
+          <div className='menu-item'>
+            <p>Earl Grey Milk Tea</p>
+            <p>$5.99</p>
+          </div>
+          <div className='menu-item'>
+            <p>Earl Grey Milk Tea</p>
+            <p>$5.99</p>
+          </div>
+          <div className='menu-item'>
+            <p>Mango Milk Tea</p>
+            <p>$5.99</p>
+          </div>
+          <div className='menu-item'>
+            <p>Oolong Milk Tea</p>
+            <p>$5.99</p>
+          </div>
+          <div className='menu-item'>
+            <p>Strawberry Milk Tea</p>
+            <p>$5.99</p>
+          </div>
+        </section>
+      </section>
+    </div>
   )
 }
 
