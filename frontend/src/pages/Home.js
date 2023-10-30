68a1f79d77354c7cfee9ee79ba72ac89488dda8a
briancoco <briannguyencoco@gmail.com>
2023-10-30 16:06:10 -0500

    Completed home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Tea1 from '../assets/Tea-1.png';
+import Tea2 from '../assets/Tea-2.jpg';
+import Tea3 from '../assets/Tea-3.png'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <h1>Bringing Bubble Tea to the United States!</h1>
+      <section className='home-img-container'>
+        <img className='home-img' src={Tea1} />
+        <img className='home-img' src={Tea2} />
+        <img className='home-img' src={Tea3} />
+      </section>
+      <button className='order-btn'>Order Now</button>
+
+    </div>
   )
 }
 
