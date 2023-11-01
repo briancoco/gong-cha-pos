bc68c2775da6eed413e64b4a750e055758f27f74
briancoco <briannguyencoco@gmail.com>
2023-10-31 20:28:30 -0500

    Added new components for ordering system and linked them to our router

---
+++ frontend/src/components/DisplayItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DisplayItem = () => {
+  return (
+    <div>DisplayItem</div>
+  )
+}
+
+export default DisplayItem
---
+++ frontend/src/pages/Drink.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Drink = () => {
+  return (
+    <div>Drink</div>
+  )
+}
+
+export default Drink
