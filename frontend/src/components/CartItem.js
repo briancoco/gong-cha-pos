e01dc7748b4972683d41abb60c498fb0423bff94
briancoco <briannguyencoco@gmail.com>
2023-11-02 12:34:59 -0500

    added remove cart functionality and empty cart handling

--- frontend/src/components/CartItem.js
+++ frontend/src/components/CartItem.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const CartItem = ({itemName, itemImage}) => {
+const CartItem = ({itemName, itemImage, onRemove}) => {
   return (
     <div className='cart-item'>
       <img className='cart-item-img' src={itemImage} alt="" />
       <h5>{itemName}</h5>
       <section className='cart-item-info'>
         <div>$5.99</div>
-        <button className='remove-btn'>Remove</button>
+        <button className='remove-btn' onClick={onRemove}>Remove</button>
       </section>
     </div>
   )
--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,24 +1,46 @@
 import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom';
 import CartItem from '../components/CartItem';
 
 const Cart = () => {
   const [cart, setCart] = useState([]);
+
   useEffect(() => {
     let currCart = localStorage.getItem('cart');
     if(!currCart) return;
     setCart(JSON.parse(currCart));
   }, []);
 
+  const removeCart = (id) => {
+    let res = [...cart];
+    res = res.filter((cartItem) => cartItem.drink_id !== id);
+    localStorage.setItem('cart', JSON.stringify(res));
+    setCart(res);
+  };
+
+  const handleCheckout = () => {
+    setCart([]);
+    localStorage.removeItem('cart');
+  }
+
   return (
     <div className='cart-container'>
-      <div className='cart'>
-        {
-          cart.map((cartItem, index) => (
-            <CartItem key={index} itemName={cartItem.name} itemImage={cartItem.image} />
-          ))
-        }
-      </div>
-      <button className='checkout-btn'>Checkout</button>
+      {
+        cart.length ? 
+        <div className='cart'>
+          {
+            cart.map((cartItem, index) => (
+              <CartItem key={index} itemName={cartItem.name} itemImage={cartItem.image} onRemove={() => removeCart(cartItem.drink_id)} />
+            ))
+          }
+        </div>
+        :
+        <section className='cart-empty'>
+          <h1>Your cart is empty</h1>
+          <Link to='/order'><button className='order-btn'>Add Items</button></Link>
+        </section>
+      }
+      {cart.length > 0 && <button className='checkout-btn' onClick={handleCheckout}>Checkout</button>}
     </div>
   )
 }
